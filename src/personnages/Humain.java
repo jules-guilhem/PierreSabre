a7a5abfb7e78594a69e31691fa0c268728c8eb1d
Jules Guilhem <glj5615a@ensfsi.onmicrosoft.com>
2026-04-16 11:11:14 +0200

    le clan des Yakuzas”

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,10 +1,13 @@
 package personnages;
 
 public class Humain {
+	private static int TAILLE_MEMOIRE = 30;
 	private String nom;
 	private String boissonFavorite;
 	private int argent;
-	
+	protected Humain[] memoire = new Humain[TAILLE_MEMOIRE];
+	protected int nbConnaissance = 0;
+
 	public Humain(String nom, String boissonFavorite, int argent) {
 		this.nom = nom;
 		this.boissonFavorite = boissonFavorite;
@@ -18,41 +21,87 @@ public class Humain {
 	public int getArgent() {
 		return argent;
 	}
-	
 
 	public void direBonjour() {
-		parler("Bonjour ! Je m'apelle "+ nom +" et j'aime boire du "+ boissonFavorite);
+		parler("Bonjour ! Je m'apelle " + nom + " et j'aime boire du " + boissonFavorite);
 	}
-	
-	
+
 	public void boire() {
-		parler("Mmmm, un bon verre de "+ boissonFavorite +" ! GLOUPS !");
+		parler("Mmmm, un bon verre de " + boissonFavorite + " ! GLOUPS !");
 	}
-	
+
 	public void acheter(String bien, int prix) {
-		if (prix>argent) {
-			parler("Je n'ai plus que "+ argent +" sous en poche. Je ne peux même pas m'offrir "+ bien +" à "+ prix +" sous");
+		if (prix > argent) {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix
+					+ " sous");
 		} else {
-			parler("J'ai "+ argent +" sous en poche. Je vais pouvoir m'offrir "+ bien +" à "+ prix +" sous");
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous");
 			perdreArgent(prix);
 		}
-		
+
 	}
-	
-	
+
 	protected void parler(String texte) {
-		System.out.println("("+ nom +") - " + texte);
+		System.out.println("(" + nom + ") - " + texte);
 	}
-	
+
 	protected void gagnerArgent(int gain) {
 		argent += gain;
 	}
-	
+
 	protected void perdreArgent(int perte) {
 		argent -= perte;
 	}
-	
-	
-	
-	
+
+	public void faireConnaissanceAvec(Humain autrehumain) {
+		direBonjour();
+		autrehumain.repondre(this);
+		memoriser(autrehumain);
+	}
+
+	private void memoriser(Humain humain) {
+		if (nbConnaissance == TAILLE_MEMOIRE) {
+			for (int i = 0; i < memoire.length - 1; i++) {
+				Humain sauv = memoire[i + 1];
+				memoire[i + 1] = memoire[i];
+				memoire[i] = sauv;
+			}
+			memoire[nbConnaissance - 1] = humain;
+		} else {
+			if (!in(humain, memoire)) {
+				memoire[nbConnaissance] = humain;
+				nbConnaissance++;
+			}
+		}
+	}
+
+	private Boolean in(Humain humain, Humain[] t) {
+		Boolean status = false;
+		Humain test = null;
+		int i = 0;
+		while (humain != test && i < TAILLE_MEMOIRE) {
+			test = t[i];
+			if (test == humain) {
+				status = true;
+			}
+			i++;
+		}
+		return status;
+	}
+
+	private void repondre(Humain humain) {
+		direBonjour();
+		memoriser(humain);
+	}
+
+	public void listerConnaissance() {
+		String phrase = "Je connais beaucoup de monde dont : ";
+		for (Humain humain : memoire) {
+			if (humain != null) {
+				phrase += humain.getNom() + ",";
+			}
+		}
+		parler(phrase);
+	}
+
 }
--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -4,37 +4,45 @@ public class Yakuza extends Humain {
 	private String clan;
 	private int reputation = 0;
 
-	public Yakuza(String nom,String boissonFavorite, int argent, String clan) {
-		super(nom,boissonFavorite,argent);
+	public Yakuza(String nom, String boissonFavorite, int argent, String clan) {
+		super(nom, boissonFavorite, argent);
 		this.clan = clan;
 	}
-	
-	public void  extorquer(Commercant victime) {
+
+	public void extorquer(Commercant victime) {
 		parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
-		parler(victime.getNom()+", si tu tiens à la vie donne moi ta bourse !");
+		parler(victime.getNom() + ", si tu tiens à la vie donne moi ta bourse !");
 		int argentVole = victime.seFaireExtorquer();
 		gagnerArgent(argentVole);
-		parler("J’ai piqué les "+ argentVole +" sous de Marco, ce qui me fait "+ getArgent() +" sous dans ma poche. Hi ! Hi !");
+		parler("J’ai piqué les " + argentVole + " sous de Marco, ce qui me fait " + getArgent()
+				+ " sous dans ma poche. Hi ! Hi !");
 		reputation++;
 	}
 
 	public int getReputation() {
 		return reputation;
 	}
-	
+
 	protected void gagner(int gain) {
 		gagnerArgent(gain);
 		reputation++;
-		parler("Ce ronin pensait vraiment battre "+ getNom() +" du clan de "+ clan +" ? Je l'ai dépouillé de ses "+ gain +" sous.");
+		parler("Ce ronin pensait vraiment battre " + getNom() + " du clan de " + clan + " ? Je l'ai dépouillé de ses "
+				+ gain + " sous.");
 	}
-	
+
 	protected void perdre() {
 		int argentPerdu = getArgent();
 		perdreArgent(argentPerdu);
-		if (reputation>0) {
+		if (reputation > 0) {
 			reputation--;
 		}
-		parler("J’ai perdu mon duel et mes "+ argentPerdu +" sous, snif... J'ai déshonoré le clan de "+ clan);
+		parler("J’ai perdu mon duel et mes " + argentPerdu + " sous, snif... J'ai déshonoré le clan de " + clan);
+	}
+
+	@Override
+	public void direBonjour() {
+		super.direBonjour();
+		parler("Mon clan est celui de " + clan + ".");
 	}
 
 }
