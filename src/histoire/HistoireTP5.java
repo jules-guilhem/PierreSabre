6cb9332af7f3b0b64c4df0d452bf9be5d2b8ae41
jules-guilhem <julesguilhemnsi@gmail.com>
2026-04-24 10:59:38 +0200

    les traitres

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -3,6 +3,7 @@ package histoire;
 import personnages.Commercant;
 import personnages.Ronin;
 import personnages.Samourai;
+import personnages.Traitre;
 import personnages.Yakuza;
 
 public class HistoireTP5 {
@@ -15,7 +16,7 @@ public class HistoireTP5 {
 		Commercant kumi = new Commercant("Kumi", 10);
 		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
 		Ronin roro = new Ronin("Roro", "shochu", 60);
-
+		/*
 		marco.faireConnaissanceAvec(roro);
 		marco.faireConnaissanceAvec(yaku);
 		marco.faireConnaissanceAvec(chonin);
@@ -23,11 +24,23 @@ public class HistoireTP5 {
 		marco.listerConnaissance();
 		roro.listerConnaissance();
 		yaku.listerConnaissance();
-
+	    */
 		Samourai akimoto = new Samourai("Miyamoto", "Akimoto", "saké", 80);
+		/*
 		akimoto.faireConnaissanceAvec(marco);
 		akimoto.listerConnaissance();
 		akimoto.boire("the");
+		*/
+		Traitre masako = new Traitre("Miyamoto", "Masako", "whisky", 100);
+		masako.faireLeGentil();
+		masako.ranconner(kumi);
+		masako.ranconner(chonin);
+		masako.ranconner(marco);
+		akimoto.faireConnaissanceAvec(masako);
+		masako.ranconner(kumi);
+		masako.faireConnaissanceAvec(yaku);
+		masako.faireLeGentil();
+		masako.faireConnaissanceAvec(roro);
 
 	}
 
--- src/personnages/Traitre.java
+++ src/personnages/Traitre.java
@@ -40,6 +40,14 @@ public class Traitre extends Samourai {
 			String nomAmi = ami.getNom();
 			parler("Il faut absolument remonter ma cote de confiance. Je vais faire ami ami avec " + nomAmi + ".");
 			parler("Bonjour l'ami ! Je voudrais vous aider en vous donnant " + don + " sous.");
+			ami.gagnerArgent(don);
+			perdreArgent(don);
+			String nom = getNom();
+			ami.parler("Merci "+ nom +". Vous êtes quelqu'un de bien.");
+			
+			if (niveauTraitrise>1) {
+				niveauTraitrise--;
+			}
 
 		}
 	}
